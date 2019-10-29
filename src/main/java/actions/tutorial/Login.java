afd2397726523b934b678da88aa9bd5b8c395e46
OnlyKasa <quangnguyen6295@gmail.com>
2019-10-29 23:59:49 +0700

    create action by annotion

--- src/main/java/actions/tutorial/Login.java
+++ src/main/java/actions/tutorial/Login.java
@@ -1,8 +1,14 @@
 package actions.tutorial;
 
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+
+@Action("tutorial")
+@Results(@Result(name = "success", location="/tutorial/login/success.jsp"))
 public class Login {
 
     public String execute() throws Exception {
-        return "error";
+        return "success";
     };
 }
